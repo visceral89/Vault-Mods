2f3466faf06f3e96ab26064970d873b156911029
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-11-04 10:50:15 +0100

    Defining font styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-	return <div className={styles.page}></div>;
+	return (
+		<main>
+			<div className={styles.divpadding}>
+				<section className={styles.textContainer}>
+					<p>SUBHEADING</p>
+					<h1>Lorem ipsum dolor sit amet.</h1>
+					<div className={styles.textFrame}>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiusmod tempor
+							incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+							exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+						</p>
+					</div>
+				</section>
+			</div>
+		</main>
+	);
 }
