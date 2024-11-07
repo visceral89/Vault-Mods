1d009449df21bdde4493e0bf0327eda90ca28b97
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-11-07 14:16:35 +0100

    working on icons

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -9,13 +9,50 @@ export const IconHamburgerMenu = () => {
 			{isHover ? (
 				<svg
 					xmlns='http://www.w3.org/2000/svg'
+					fill='none'
+					viewBox='0 0 24 24'
+					strokeWidth={1.5}
+					stroke='#26bbff'
+					className='size-6'>
+					<path
+						strokeLinecap='round'
+						strokeLinejoin='round'
+						d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+					/>
+				</svg>
+			) : (
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					fill='none'
+					viewBox='0 0 24 24'
+					strokeWidth={1.5}
+					stroke='#ffffff'
+					className='size-6'>
+					<path
+						strokeLinecap='round'
+						strokeLinejoin='round'
+						d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+					/>
+				</svg>
+			)}
+		</div>
+	);
+};
+
+// Fråga Magnus eller Jon ifall man kan på snyggare sätt ändra FILL på svg'n
+
+/* OLD
+
+					<svg
+					xmlns='http://www.w3.org/2000/svg'
 					height='24px'
 					viewBox='0 -960 960 960'
 					width='24px'
 					fill='#26bbff'>
 					<path d='M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z' />
 				</svg>
-			) : (
+
+
 				<svg
 					xmlns='http://www.w3.org/2000/svg'
 					height='24px'
@@ -24,9 +61,4 @@ export const IconHamburgerMenu = () => {
 					fill='#ffffff'>
 					<path d='M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z' />
 				</svg>
-			)}
-		</div>
-	);
-};
-
-// Fråga Magnus eller Jon ifall man kan på snyggare sätt ändra FILL på svg'n
+					*/
--- app/components/navbar/Hamburger.tsx
+++ app/components/navbar/Hamburger.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useState } from "react";
 import styles from "./Hamburger.module.scss";
+import { IconHamburgerMenu } from "../Icons";
 
 type HamburgerProps = {
 	children: ReactNode;
@@ -14,7 +15,9 @@ export default function Hamburger({ children }: HamburgerProps) {
 
 	return (
 		<div className={styles.hamburgerMenu}>
-			{/* icon */}
+			<div onClick={handleOpen}>
+				<IconHamburgerMenu />
+			</div>
 			{isOpen && <div className={styles.background} onClick={() => setIsOpen(false)}></div>}
 			<div className={`${styles.menuLinks} ${isOpen ? styles.menuLinksOpen : ""}`}>{children}</div>
 		</div>
